2b72d1ea0f5926f6291297f41722af80ef2e157d
ronaldemanuel <emronald15el@gmail.com>
2022-02-02 19:20:43 -0300

    feat: implement product CRUD

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -10,33 +10,37 @@ import {
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './product.entity';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
-  }
-
   @Get()
-  findAll() {
-    return this.productService.findAll();
+  async findAll(): Promise<Product[]> {
+    return await this.productService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Product> {
+    return await this.productService.findOne(+id);
+  }
+
+  @Post()
+  async create(@Body() data: CreateProductDto): Promise<Product> {
+    return await this.productService.create(data);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  async update(
+    @Param('id') id: string,
+    @Body() data: UpdateProductDto,
+  ): Promise<Product> {
+    return await this.productService.update(+id, data);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  async remove(@Param('id') id: string): Promise<void> {
+    return await this.productService.remove(+id);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './product.entity';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async findAll(): Promise<Product[]> {
+    const products = await this.productRepository.find();
+    return products;
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findOne(id: number): Promise<Product> {
+    const product = await this.productRepository.findOne({ id });
+    if (!product) {
+      throw new NotFoundException('product not found');
+    }
+    return product;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async create(data: CreateProductDto): Promise<Product> {
+    const template = this.productRepository.create(data);
+    const saved = this.productRepository.save(template);
+    if (!saved) {
+      throw new InternalServerErrorException('fail to create product');
+    }
+    return saved;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, data: UpdateProductDto): Promise<Product> {
+    const product = await this.findOne(id);
+    const updated = await this.productRepository.save(
+      Object.assign(product, data),
+    );
+    return updated;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number): Promise<void> {
+    const deleted = await this.productRepository.delete(id);
+    if (!deleted) {
+      throw new InternalServerErrorException('product not deleted');
+    } else {
+      true;
+    }
   }
 }
