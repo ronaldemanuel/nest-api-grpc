44206097e52c63f7fcb74753b88dd9e18cbaae28
ronaldemanuel <emronald15el@gmail.com>
2022-02-07 00:41:59 -0300

    feat: implement update method on product grpc controller and proto

--- src/product/product-grpc-server/product-grpc-server.controller.ts
+++ src/product/product-grpc-server/product-grpc-server.controller.ts
@@ -2,6 +2,7 @@ import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { CreateProductDto } from '../dto/create-product.dto';
+import { UpdateProductDto } from '../dto/update-product.dto';
 import { Product } from '../product.entity';
 import { ProductService } from '../product.service';
 
@@ -12,11 +13,15 @@ export class ProductGrpcServerController {
   @GrpcMethod('ProductService', 'Create')
   create(
     data: CreateProductDto,
-    metadata: Metadata,
-    call: ServerUnaryCall<CreateProductDto, Product>,
+    // metadata: Metadata,
+    // call: ServerUnaryCall<CreateProductDto, Product>,
   ) {
-    console.log({ data, metadata, call });
-
     return this.productService.create(data);
+  }
+
+  @GrpcMethod('ProductService', 'Update')
+  update(data: { id: number; name: string; price: number }) {
+    const { id, ...req } = data;
+    return this.productService.update(id, req);
   }
 }
